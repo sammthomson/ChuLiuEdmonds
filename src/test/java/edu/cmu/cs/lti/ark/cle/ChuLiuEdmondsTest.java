230cac93d7fa3825a09aa8324e3c9a69d0464899
Sam Thomson <sammthomson@gmail.com>
2014-03-26 23:35:14 -0400

    start of a diverse k-best implementation

--- src/test/java/edu/cmu/cs/lti/ark/cle/ChuLiuEdmondsTest.java
+++ src/test/java/edu/cmu/cs/lti/ark/cle/ChuLiuEdmondsTest.java
@@ -2,6 +2,7 @@ package edu.cmu.cs.lti.ark.cle;
 
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -63,6 +64,72 @@ public class ChuLiuEdmondsTest {
 		assertEquals(2, maxBranching.get(4).intValue());
 		assertEquals(90.0, weightedSpanningTree.weight, DELTA);
 		assertEdgesSumToScore(weights, weightedSpanningTree);
+	}
+
+	@Test
+	public void testGetDiverseKBestMaxSpanningTrees() {
+		/*
+		root    10
+		(0) -------> (1) <
+		 |     ---^   |  \
+		 |10  /        |10 \
+		 |   /20        \   \ 20
+		 |  /            \   \
+		 V /  35           V  |
+		(3)<----- (2) <----- (4)
+		  \-------^     50
+		     40
+		 */
+		double[][] weights = {
+				{NINF, 10, NINF, 10, NINF},
+				{NINF, NINF, NINF, NINF, 10 },
+				{NINF, NINF, NINF,  35, NINF},
+				{NINF, 20, 40, NINF, NINF},
+				{NINF, 20, 50, NINF, NINF},
+		};
+		final List<Weighted<Map<Integer, Integer>>> weightedSpanningTrees =
+				ChuLiuEdmonds.getDiverseKBestSpanningTrees(weights, 0, 2, 50.0);
+		for (Weighted<Map<Integer, Integer>> weightedSpanningTree : weightedSpanningTrees) {
+			printTree(weightedSpanningTree);
+		}
+		/*
+		root    10
+		(0) -------> (1)
+		               |
+		               |10
+		                \
+		                 \
+		      35           V
+		(3)<----- (2) <----- (4)
+		                50
+		 */
+		final Weighted<Map<Integer, Integer>> weightedSpanningTree = weightedSpanningTrees.get(0);
+		Map<Integer, Integer> maxBranching = weightedSpanningTree.val;
+		assertEquals(0, maxBranching.get(1).intValue());
+		assertEquals(4, maxBranching.get(2).intValue());
+		assertEquals(2, maxBranching.get(3).intValue());
+		assertEquals(1, maxBranching.get(4).intValue());
+		assertEquals(105.0, weightedSpanningTree.weight, DELTA);
+		assertEdgesSumToScore(weights, weightedSpanningTree);
+		/*
+		root
+		(0)         (1)
+		 |     ---^   |
+		 |10  /        |-40 (10 - 50)
+		 |   /20        \
+		 |  /            \
+		 V /               V
+		(3)       (2)       (4)
+		  \-------^
+		     40
+		 */
+		maxBranching = weightedSpanningTrees.get(1).val;
+		assertEquals(3, maxBranching.get(1).intValue());
+		assertEquals(3, maxBranching.get(2).intValue());
+		assertEquals(0, maxBranching.get(3).intValue());
+		assertEquals(1, maxBranching.get(4).intValue());
+		assertEquals(30.0, weightedSpanningTrees.get(1).weight, DELTA);
+
 	}
 
 	private void printTree(Weighted<Map<Integer, Integer>> weightedSpanningTree) {
