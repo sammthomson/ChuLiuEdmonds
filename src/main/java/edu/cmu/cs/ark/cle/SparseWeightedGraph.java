61da2a7f8ac089bca24ae0fdd3eb404cd8b6503d
Sam Thomson <sammthomson@gmail.com>
2014-06-10 12:17:06 -0400

    outgoingEdges was unnecessary

--- src/main/java/edu/cmu/cs/ark/cle/SparseWeightedGraph.java
+++ src/main/java/edu/cmu/cs/ark/cle/SparseWeightedGraph.java
@@ -13,31 +13,22 @@ import java.util.Set;
  */
 public class SparseWeightedGraph<V> extends WeightedGraph<V> {
 	final private Set<V> nodes;
-	final private Map<V, Map<V, Double>> outgoingEdges;
 	final private Map<V, Map<V, Weighted<Edge<V>>>> incomingEdges;
 
-	private SparseWeightedGraph(Set<V> nodes,
-								Map<V, Map<V, Double>> outgoingEdges,
-								Map<V, Map<V, Weighted<Edge<V>>>> incomingEdges) {
+	private SparseWeightedGraph(Set<V> nodes, Map<V, Map<V, Weighted<Edge<V>>>> incomingEdges) {
 		this.nodes = nodes;
-		this.outgoingEdges = outgoingEdges;
 		this.incomingEdges = incomingEdges;
 	}
 
 	public static <T> SparseWeightedGraph<T> from(Iterable<T> nodes, Iterable<Weighted<Edge<T>>> edges) {
-		final Map<T, Map<T, Double>> outgoingEdges = Maps.newHashMap();
 		final Map<T, Map<T, Weighted<Edge<T>>>> incomingEdges = Maps.newHashMap();
 		for (Weighted<Edge<T>> edge : edges) {
-			if (!outgoingEdges.containsKey(edge.val.source)) {
-				outgoingEdges.put(edge.val.source, Maps.<T, Double>newHashMap());
-			}
-			outgoingEdges.get(edge.val.source).put(edge.val.destination, edge.weight);
 			if (!incomingEdges.containsKey(edge.val.destination)) {
 				incomingEdges.put(edge.val.destination, Maps.<T, Weighted<Edge<T>>>newHashMap());
 			}
 			incomingEdges.get(edge.val.destination).put(edge.val.source, edge);
 		}
-		return new SparseWeightedGraph<T>(ImmutableSet.copyOf(nodes), outgoingEdges, incomingEdges);
+		return new SparseWeightedGraph<T>(ImmutableSet.copyOf(nodes), incomingEdges);
 	}
 
 	public static <T> SparseWeightedGraph<T> from(Iterable<Weighted<Edge<T>>> edges) {
@@ -56,10 +47,10 @@ public class SparseWeightedGraph<V> extends WeightedGraph<V> {
 
 	@Override
 	public double getWeightOf(V source, V dest) {
-		if (!outgoingEdges.containsKey(source)) return Double.NEGATIVE_INFINITY;
-		final Map<V, Double> outEdges = outgoingEdges.get(source);
-		if (!outEdges.containsKey(dest)) return Double.NEGATIVE_INFINITY;
-		return outEdges.get(dest);
+		if (!incomingEdges.containsKey(dest)) return Double.NEGATIVE_INFINITY;
+		final Map<V, Weighted<Edge<V>>> inEdges = incomingEdges.get(dest);
+		if (!inEdges.containsKey(source)) return Double.NEGATIVE_INFINITY;
+		return inEdges.get(source).weight;
 	}
 
 	@Override
